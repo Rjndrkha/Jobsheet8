1dc81af5ac8ca9f5720786ab4f569e23252055dd
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-10 16:24:14 +0700

    Controller & EDIT Views

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -19,7 +19,7 @@
                         @foreach ($errors->all() as $error)
 
                         <li>{{ $error }}</li>
-                        
+
                         @endforeach
 
                     </ul>
@@ -35,46 +35,38 @@
 
                     <div class="form-group">
                         <label for="Nim">Nim</label>
-                        <input type="text" name="Nim" class="form-control" id="Nim" value="{{ $Student->nim }}" ariadescribedby="Nim" >
+                        <input type="text" name="Nim" class="form-control" id="Nim" value="{{ $Student->nim }}" ariadescribedby="Nim">
                     </div>
                     <div class="form-group">
                         <label for="Name">Name</label>
-                        <input type="text" name="Name" class="form-control" id="Name" value="{{ $Student->name }}" aria-describedby="Name" >
-                            </div>
-                            <div class="form-group">
-                                <label for="Class">Class</label>
-                                <input type="Class" name="Class" class="form-control" id="Class" value="{{ $Student->class }}" aria-describedby="Class" >
-                            </div>
-                            <div class="form-group">
-                                <label for="Major">Major</label>
-                                <input type="Major" name="Major" class="form-control" id="Major" value="{{ $Student->major }}" aria-describedby="Major" >
-                            </div>
-                            <div class="form-group">
-                                <label for="DateOfBirth">Birth Date</label>
-                                <input type="date" name="DateOfBirth" class="form-control" id="DateOfBirth" value="{{ $Student->DateOfBirth }}" aria-describedby="DateOfBirth" >
-                            </div>
-                            <div class="form-group">
-                                <label for="Address">Address</label>
-                                <input type="Address" name="Address" class="form-control" id="Address" value="{{ $Student->Address }}" aria-describedby="Address" >
-                            </div>
-                            
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
+                        <input type="text" name="Name" class="form-control" id="Name" value="{{ $Student->name }}" aria-describedby="Name">
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="class">Class</label>
+                        <select name="class" class="form-control">
+                            @foreach($class as $kls)
+                            <option value="{{$kls->id}}" {{$Student->class_id == $kls->id ? 'selected' : ''}}>{{$kls->class_name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="Major">Major</label>
+                        <input type="Major" name="Major" class="form-control" id="Major" value="{{ $Student->major }}" aria-describedby="Major">
+                    </div>
+                    <div class="form-group">
+                        <label for="DateOfBirth">Birth Date</label>
+                        <input type="date" name="DateOfBirth" class="form-control" id="DateOfBirth" value="{{ $Student->DateOfBirth }}" aria-describedby="DateOfBirth">
+                    </div>
+                    <div class="form-group">
+                        <label for="Address">Address</label>
+                        <input type="Address" name="Address" class="form-control" id="Address" value="{{ $Student->Address }}" aria-describedby="Address">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
+    </div>
+</div>
 
-        @endsection
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+@endsection
