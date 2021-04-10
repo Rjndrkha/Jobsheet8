f740b7edec4ecce83a744d173e0a01601d4071cb
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-10 14:14:59 +0700

    SEEDER&MIGRATION

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class ClassSeeder extends Seeder
 {
     /**
@@ -13,6 +13,18 @@ class ClassSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $class = [
+            ['class_name' => 'TI 2A',],
+            ['class_name' => 'TI 2B',],
+            ['class_name' => 'TI 2C',],
+            ['class_name' => 'TI 2D',],
+            ['class_name' => 'TI 2E',],
+            ['class_name' => 'TI 2F',],
+            ['class_name' => 'TI 2G',],
+            ['class_name' => 'TI 2H',],
+            ['class_name' => 'TI 2I',],
+        ];
+
+        DB::table('class')->insert($class);
     }
 }
