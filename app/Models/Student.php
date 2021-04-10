34433c067b8f16a373ee9164914481785d940ac4
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-10 15:23:52 +0700

    ADD

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,14 +1,13 @@
 <?php
 
 namespace App\Models;
-
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\ClassModel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\Student as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 
 class Student extends Model
 {
